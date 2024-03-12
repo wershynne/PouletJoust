3d712d1dfbe64f8e5eac2eac09406b670138ac8b
wershynne <111524741+wershynne@users.noreply.github.com>
2024-03-12 15:17:13 -0700

    Add files via upload

---
+++ Sub.gd
@@ -0,0 +1,22 @@
+extends MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+var move=0
+func _process(delta):
+	if Input.is_action_pressed("ui_left"):
+		move=0.05
+		position.x-=move
+		rotate_y(+move/50)
+		move=0
+	if Input.is_action_pressed("ui_right"):
+		move=0.05
+		position.x+=move
+		rotate_y(-move/50)
+		move=0
+	pass
